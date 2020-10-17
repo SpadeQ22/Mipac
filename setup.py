179bbad4736ed03643f3c0aef7d223ebe918f719
SpadeQ22 <67244800+SpadeQ22@users.noreply.github.com>
2020-10-17 15:36:20 +0200

    Editing Some Code Malfunctions

---
+++ setup.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.call("pip3 install netfilterqueue", shell=True)
+subprocess.call("pip3 install scapy", shell=True)
+subprocess.call("pip3 install twisted", shell=True)
+subprocess.call("pip3 install maclookup", shell=True)
